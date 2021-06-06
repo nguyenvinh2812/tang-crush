f75f3639d37452b971fc0d0274e230e89a1ec737
nguyenvinh2812 <85452307+nguyenvinh2812@users.noreply.github.com>
2021-06-07 00:00:35 +0700

    b

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Babiiii à!',
+    introDesc: `Hum nay em đau bụng
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
@@ -12,8 +9,9 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Anh biết em khổ lắm:(((.
+    messDesc: cố chịu thêm chút nứa nhaaaa.'
+    anh bít phải làm gì rùi .Sáng mai anh qua nhaaaa
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
