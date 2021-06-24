7fb3cdd6166f61148f91e5ae422c3f21e2801af4
nguyenvinh2812 <85452307+nguyenvinh2812@users.noreply.github.com>
2021-06-24 10:29:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babiiiiii của anh à!',
-    introDesc: `Hôm nay em đau bụng
-    Mà anh không thể làm được gì cho em
-    hicccc sr em iu nhìu nhaaa
-    cố lên nhaaa`,
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Zừa anh rớt liêm sỉ join các hội ce để xem review 😙',
-    desc: 'Em iu thấy anh giỏi honggg ',
-    btnYes: 'Anh iu giỏi quaaaaa <33',
-    btnNo: 'Say no :3',
-    mess: 'Bây giờ anh biết phải làm gì rùi 🥰.Sẽ có bất ngờ cho em đấy.Babiii cố thêm chút xíu nhaaaa. ',
-    messDesc: 'Mai gặp lại em iu nhóo.Yêu em nhiều nhiều 😘😘',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com/profile.php?id=100032467463450' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
